31d2e155070282165a39c7849084efe165f216e6
Paul Portier <paulportier@gmail.com>
2024-02-24 21:23:03 +0100

    update schema

--- db/migrate/20200703163653_add_win_to_tirages.rb
+++ db/migrate/20200703163653_add_win_to_tirages.rb
@@ -1,5 +1,5 @@
 class AddWinToTirages < ActiveRecord::Migration[6.0]
   def change
-    add_column :tirages, :won_by, :integer
+    add_column :tirages, :winners, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,18 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema.define(version: 2023_10_14_101753) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "draws", force: :cascade do |t|
@@ -10,7 +24,7 @@ ActiveRecord::Schema.define(version: 2023_10_14_101753) do
     t.integer "number5"
     t.integer "star1"
     t.integer "star2"
-    t.integer "won_by"
+    t.integer "winners"
     t.string "prize"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -27,4 +41,5 @@ ActiveRecord::Schema.define(version: 2023_10_14_101753) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
 end
