80954cea3b17f7ff21b4597f7ce37198acf276f5
Paul Portier <paulportier@gmail.com>
2024-02-24 21:27:14 +0100

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,8 @@ class PagesController < ApplicationController
 
   def home
     @draw_count = Draw.count
-    @won_draw_count = Draw.where.not(won_by: nil).count
+    @won_draw_count = Draw.where.not(winners: nil).count
     @won_draw_percentage = (@won_draw_count.to_f / @draw_count.to_f * 100).round(2)
-    @winners_count = Draw.where.not(won_by: nil).sum(:won_by)
+    @winners_count = Draw.where.not(winners: nil).sum(:winners)
   end
 end
